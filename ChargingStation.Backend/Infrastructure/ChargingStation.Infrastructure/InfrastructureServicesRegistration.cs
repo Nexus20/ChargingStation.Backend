7a87300a943e2a16e4551352c7db769f16a58c91
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2024-01-28 16:30:46 +0200

    Added base API project. Added migrations

---
+++ ChargingStation.Backend/Infrastructure/ChargingStation.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ChargingStation.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChargingStation.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        
+        // services.AddTransient<IUnitOfWork, UnitOfWork>();
+        // services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        
+        return services;
+    }
+}
---
+++ ChargingStation.Backend/Infrastructure/ChargingStation.Infrastructure/Migrations/20240128142948_Depots.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChargingStation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Depots : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Depots",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Building = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    Longitude = table.Column<double>(type: "float", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    EnergyLimit = table.Column<double>(type: "float", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Depots", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Depots");
+        }
+    }
+}
