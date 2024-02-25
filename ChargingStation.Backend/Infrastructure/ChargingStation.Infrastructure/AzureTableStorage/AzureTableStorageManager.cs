1437345b6abd55e6a8d4fb0d9f53ef47efc81b41
Denis <denisrubel40@gmail.com>
2024-02-25 19:34:21 +0200

    Added helper Сlasses to work with Azure Table Storage

---
+++ ChargingStation.Backend/Infrastructure/ChargingStation.Infrastructure/AzureTableStorage/AzureTableStorageManager.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Data.Tables;
+
+namespace ChargingStation.Infrastructure.AzureTableStorage
+{
+    public class AzureTableStorageManager<T> where T : BaseTableStorageEntity
+    {
+        private readonly TableServiceClient _tableServiceClient;
+        private readonly string _tableName;
+
+        public AzureTableStorageManager(string connectionString, string tableName)
+        {
+            _tableServiceClient = new TableServiceClient(connectionString);
+            _tableName = tableName;
+        }
+
+        public async Task AddEntityAsync(T entity)
+        {
+            var tableClient = GetTableClient();
+            await tableClient.AddEntityAsync(entity);
+        }
+
+        public async Task<T> GetEntityAsync(string partitionKey, string rowKey)
+        {
+            var tableClient = GetTableClient();
+            return await tableClient.GetEntityAsync<T>(partitionKey, rowKey);
+        }
+
+        public async Task<IEnumerable<T>> GetEntitiesByPartitionKeyAsync(string partitionKey)
+        {
+            var tableClient = GetTableClient();
+            return await tableClient.QueryAsync<T>(filter: $"PartitionKey eq '{partitionKey}'").ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllEntitiesAsync()
+        {
+            var tableClient = GetTableClient();
+            return await tableClient.QueryAsync<T>().ToListAsync();
+        }
+
+        public async Task UpdateEntityAsync(T entity)
+        {
+            var tableClient = GetTableClient();
+            await tableClient.UpsertEntityAsync(entity);
+        }
+
+        public async Task DeleteEntityAsync(T entity)
+        {
+            var tableClient = GetTableClient();
+            await tableClient.DeleteEntityAsync(entity.PartitionKey, entity.RowKey);
+        }
+
+
+        private TableClient GetTableClient()
+        {
+            return _tableServiceClient.GetTableClient(_tableName);
+        }
+    }
+}
---
+++ ChargingStation.Backend/Infrastructure/ChargingStation.Infrastructure/AzureTableStorage/BaseTableStorageEntity.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ChargingStation.Infrastructure.AzureTableStorage
+{
+    public class BaseTableStorageEntity : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        BaseTableStorageEntity(string partitionKey, string rowKey, DateTimeOffset? timestamp, ETag eTag)
+        {
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+            Timestamp = timestamp;
+            ETag = eTag;
+        }
+    }
+}
