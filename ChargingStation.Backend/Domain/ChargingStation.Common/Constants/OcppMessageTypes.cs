084711e9ff640a3d7922cf162f4e8aa903df6c3b
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2024-03-06 19:59:19 +0200

    Ocpp constants

---
+++ ChargingStation.Backend/Domain/ChargingStation.Common/Constants/OcppMessageTypes.cs
@@ -0,0 +1,8 @@
+namespace ChargingStation.Common.Constants;
+
+public static class OcppMessageTypes
+{
+    public const string Call = "2";
+    public const string CallResult = "3";
+    public const string CallError = "4";
+}
