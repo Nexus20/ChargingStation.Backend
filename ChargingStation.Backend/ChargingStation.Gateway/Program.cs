a9ce536391e755c8ea8e27ff711e9501f90350d8
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2024-04-15 22:27:58 +0300

    Added cors

--- ChargingStation.Backend/ChargingStation.Gateway/Program.cs
+++ ChargingStation.Backend/ChargingStation.Gateway/Program.cs
@@ -5,6 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllPolicy",
+        b => b.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+});
 
 var app = builder.Build();
 
@@ -13,6 +21,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
     app.UseDeveloperExceptionPage();
 }
 
+app.UseCors("AllowAllPolicy");
 await app.UseOcelot();
 
 app.Run();
