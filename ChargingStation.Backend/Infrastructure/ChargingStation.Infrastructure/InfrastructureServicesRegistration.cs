6fefd19299695bd24e9df00f796177728928c298
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2024-05-23 01:02:02 +0300

    Emulator fixes for container work

--- ChargingStation.Backend/Infrastructure/ChargingStation.Infrastructure/InfrastructureServicesRegistration.cs
+++ ChargingStation.Backend/Infrastructure/ChargingStation.Infrastructure/InfrastructureServicesRegistration.cs
@@ -2,6 +2,7 @@
 using ChargingStation.Infrastructure.Persistence;
 using ChargingStation.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +16,14 @@ public static class InfrastructureServicesRegistration
     {
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        
+        services.AddIdentity<InfrastructureUser, InfrastructureRole>()
+            .AddUserStore<UserStore<InfrastructureUser, InfrastructureRole, ApplicationDbContext, string, IdentityUserClaim<string>, InfrastructureUserRole,
+                IdentityUserLogin<string>, IdentityUserToken<string>, IdentityRoleClaim<string>>>()
+            .AddRoleStore<RoleStore<InfrastructureRole, ApplicationDbContext, string, InfrastructureUserRole, IdentityRoleClaim<string>>>()
+            .AddSignInManager<SignInManager<InfrastructureUser>>()
+            .AddRoleManager<RoleManager<InfrastructureRole>>()
+            .AddUserManager<UserManager<InfrastructureUser>>();
 
         services.AddScoped<RoleManager<InfrastructureRole>>();
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
