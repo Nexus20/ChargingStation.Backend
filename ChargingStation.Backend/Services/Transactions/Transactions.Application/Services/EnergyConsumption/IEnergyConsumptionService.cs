cb61e6bd6ed85cc441f0c4a6714816ae58b7450d
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2024-06-02 22:08:11 +0300

    Started work on models

--- ChargingStation.Backend/Services/Transactions/Transactions.Application/Services/EnergyConsumption/IEnergyConsumptionService.cs
+++ ChargingStation.Backend/Services/Transactions/Transactions.Application/Services/EnergyConsumption/IEnergyConsumptionService.cs
@@ -4,3 +4,35 @@ public interface IEnergyConsumptionService
 {
     
 }
+
+public class GetChargePointsEnergyConsumptionByDepotRequest
+{
+    public required DateTime StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    
+    public required Guid DepotId { get; set; }
+}
+
+public class ChargePointsEnergyConsumptionResponse
+{
+    public required DateTime StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    
+    public required List<ChargePointEnergyConsumptionResponse> ChargePointsConsumption { get; set; }
+}
+
+public class ChargePointEnergyConsumptionResponse
+{
+    public required Guid ChargePointId { get; set; }
+    public required string ChargePointName { get; set; }
+    public required decimal EnergyConsumed { get; set; }
+    
+    public required List<ChargePointConnectorEnergyConsumptionResponse>
+}
+
+public class ChargePointConnectorEnergyConsumptionResponse
+{
+    public required Guid ConnectorId { get; set; }
+    public required int ConnectorNumber { get; set; }
+    public required decimal EnergyConsumed { get; set; }
+}
